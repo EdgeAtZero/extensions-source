146e5a46dd89961f7d81a51e15bd301163f6f267
EdgeAtZero <edgeatzero@gmail.com>
2025-05-27 23:04:59 +0800

    Chore: Change the build script for copy20

--- src/zh/copy20/build.gradle.kts
+++ src/zh/copy20/build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
     extra["group"] = project.name.split("-").let { "${rootProject.group}.extension.${it[1]}.${it[2]}" }
     extra["version.code"] = Const.Git.commit(project.projectDir.absolutePath)
-    extra["version.name"] = "1.4.${extra["version.code"]}"
+    extra["version.name"] = "1.4.${extra["version.code"] as Int}"
     extra["extension.name"] = "Copy20"
     extra["extension.class"] = ".Copy20"
     extra["extension.nsfw"] = true
